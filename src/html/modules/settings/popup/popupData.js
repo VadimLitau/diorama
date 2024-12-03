baec8ce9af1e259c89b19b1383bd55d6cbf90df2
VadimLitau <vadimlitau@yandex.ru>
2024-12-03 18:27:57 +0500

    add new text and images

--- src/html/modules/settings/popup/popupData.js
+++ src/html/modules/settings/popup/popupData.js
@@ -7,22 +7,26 @@ const popupData = [
 			"./images/M_001.webp",
 			"./images/M_002.webp",
 			"./images/M_003.webp",
+			"./images/M_004.webp",
+			"./images/M_005.webp",
 			"./images/COLECTION_INTERIER.webp",
 			"./images/M_INTERIER.webp",
 		],
 		oldPrice: "3500 руб.",
 		newPrice: "1650 руб.",
-		description: `Lorem ipsum dolor sit amet consectetur. Nam pellentesque amet
-					at&nbsp;quis bibendum. Duis feugiat et&nbsp;orci posuere velit
-					malesuada tempus. Ut&nbsp;vitae odio urna elementum urna enim.
-					Ut&nbsp;nisi diam purus augue id&nbsp;interdum malesuada sed massa.
-					Augue vitae volutpat nunc nisi fermentum in&nbsp;convallis orci.
-					Condimentum mattis eu&nbsp;pharetra sem quam facilisis&nbsp;id.
-					<br />
-					<br />
-					Ut&nbsp;nisi diam purus augue id&nbsp;interdum malesuada sed massa.
-					Augue vitae volutpat nunc nisi fermentum in&nbsp;convallis orci.
-					Condimentum mattis eu&nbsp;pharetra sem quam facilisis&nbsp;id.`,
+		description: `Яркий мир Super Mario отправит вас в путешествие захватывающих воспоминаний! Чтобы спасти принцессу Пич от злого Боузера, проходить уровни становилось сложнее и сложнее, прыгая по платформам, собирая монеты, уничтожая врагов и открывая секретные уровни. Соберите детали, чтобы воссоздать культовый сказочный мир пикселей и навсегда оставивший след в сердцах геймеров всех возрастов.
+		<br />
+		<br />
+		Размер упаковки : 182х202х16 мм
+		<br />
+		Размер диорамы : 200х180х19мм
+		<br />
+		Количество деталей : 73 шт.
+		<br />
+		Время сборки : 44 мин.
+		<br />
+		Уровень сложности : средний
+`,
 
 		link: "https://www.ozon.ru",
 	},
@@ -34,22 +38,25 @@ const popupData = [
 			"./images/T_001.webp",
 			"./images/T_002.webp",
 			"./images/T_003.webp",
+			"./images/T_004.webp",
+			"./images/T_005.webp",
 			"./images/COLECTION_INTERIER.webp",
 			"./images/T_INTERIER.webp",
 		],
 		oldPrice: "3500 руб.",
 		newPrice: "1650 руб.",
-		description: `Lorem ipsum dolor sit amet consectetur. Nam pellentesque amet
-					at&nbsp;quis bibendum. Duis feugiat et&nbsp;orci posuere velit
-					malesuada tempus. Ut&nbsp;vitae odio urna elementum urna enim.
-					Ut&nbsp;nisi diam purus augue id&nbsp;interdum malesuada sed massa.
-					Augue vitae volutpat nunc nisi fermentum in&nbsp;convallis orci.
-					Condimentum mattis eu&nbsp;pharetra sem quam facilisis&nbsp;id.
+		description: `Погрузитесь в&nbsp;мир стратегических танковых сражений! Соберите захватывающую арену, где ваши навыки проверяются на&nbsp;прочность. Это те&nbsp;самые &laquo;Танчики&raquo;, которые не&nbsp;оставляли равнодушным ни&nbsp;одного любителя динамичных игр на&nbsp;Dendi или Sega из&nbsp;многомиллионной аудитории. Пазл идеально подойдет для любителей классических игр, напоминая о&nbsp;захватывающих битвах на&nbsp;экране.
 					<br />
 					<br />
-					Ut&nbsp;nisi diam purus augue id&nbsp;interdum malesuada sed massa.
-					Augue vitae volutpat nunc nisi fermentum in&nbsp;convallis orci.
-					Condimentum mattis eu&nbsp;pharetra sem quam facilisis&nbsp;id.`,
+		Размер упаковки : 182х202х16 мм
+		<br />
+		Размер диорамы : 200х180х19мм
+		<br />
+		Количество деталей : 73 шт.
+		<br />
+		Время сборки : 30 мин.
+		<br />
+		Уровень сложности : средний`,
 
 		link: "https://www.ozon.ru",
 	},
@@ -61,22 +68,26 @@ const popupData = [
 			"./images/DD_001.webp",
 			"./images/DD_002.webp",
 			"./images/DD_003.webp",
+			"./images/DD_004.webp",
+			"./images/DD_005.webp",
 			"./images/COLECTION_INTERIER.webp",
 			"./images/DD_INTERIER.webp",
 		],
 		oldPrice: "3500 руб.",
 		newPrice: "1650 руб.",
-		description: `Lorem ipsum dolor sit amet consectetur. Nam pellentesque amet
-					at&nbsp;quis bibendum. Duis feugiat et&nbsp;orci posuere velit
-					malesuada tempus. Ut&nbsp;vitae odio urna elementum urna enim.
-					Ut&nbsp;nisi diam purus augue id&nbsp;interdum malesuada sed massa.
-					Augue vitae volutpat nunc nisi fermentum in&nbsp;convallis orci.
-					Condimentum mattis eu&nbsp;pharetra sem quam facilisis&nbsp;id.
+		description: `Станьте частью приключений Черного Плаща! Этот набор позволит вам воссоздать мир, где смелость и юмор идут рука об руку, где ваша миссия – это остановить злодеев и спасти город от их коварных планов, где яркий графический стиль создают атмосферу незабываемого приключения. Собирая элементы из игры любимого мультфильма, вы добавите индивидуальный штрих в оформление своего рабочего места.
 					<br />
 					<br />
-					Ut&nbsp;nisi diam purus augue id&nbsp;interdum malesuada sed massa.
-					Augue vitae volutpat nunc nisi fermentum in&nbsp;convallis orci.
-					Condimentum mattis eu&nbsp;pharetra sem quam facilisis&nbsp;id.`,
+		Размер упаковки : 182х202х16 мм
+		<br />
+		Размер диорамы : 200х180х19мм
+		<br />
+		Количество деталей : 35 шт.
+		<br />
+		Время сборки : 30 мин.
+		<br />
+		Уровень сложности : начальный
+		`,
 
 		link: "https://www.ozon.ru",
 	},
@@ -88,22 +99,26 @@ const popupData = [
 			"./images/DH_001.webp",
 			"./images/DH_002.webp",
 			"./images/DH_003.webp",
+			"./images/DH_004.webp",
+			"./images/DH_005.webp",
 			"./images/COLECTION_INTERIER.webp",
 			"./images/DH_INTERIER.webp",
 		],
 		oldPrice: "3500 руб.",
 		newPrice: "1650 руб.",
-		description: `Lorem ipsum dolor sit amet consectetur. Nam pellentesque amet
-					at&nbsp;quis bibendum. Duis feugiat et&nbsp;orci posuere velit
-					malesuada tempus. Ut&nbsp;vitae odio urna elementum urna enim.
-					Ut&nbsp;nisi diam purus augue id&nbsp;interdum malesuada sed massa.
-					Augue vitae volutpat nunc nisi fermentum in&nbsp;convallis orci.
-					Condimentum mattis eu&nbsp;pharetra sem quam facilisis&nbsp;id.
-					<br />
-					<br />
-					Ut&nbsp;nisi diam purus augue id&nbsp;interdum malesuada sed massa.
-					Augue vitae volutpat nunc nisi fermentum in&nbsp;convallis orci.
-					Condimentum mattis eu&nbsp;pharetra sem quam facilisis&nbsp;id.`,
+		description: `Возвращайтесь к классике с этим потрясающим набором, который перенесет вас в мир охоты на уток! Помните, как тренировали свою меткость, стреляя с помощью светового пистолета? Когда утки становились хитрее, а собака, собирая уже сбитых уток добавляла игре особый шарм? Соберите сцену, где точность и ловкость станут союзниками. Этот набор точно напомнит о забавных моментах с пистолетом в руках.
+		<br />
+		<br />
+		Размер упаковки : 182х202х16 мм
+		<br />
+		Размер диорамы : 200х180х19мм
+		<br />
+		Количество деталей : 29 шт.
+		<br />
+		Время сборки : 30 мин.
+		<br />
+		Уровень сложности : начальный
+`,
 
 		link: "https://www.ozon.ru",
 	},
@@ -115,22 +130,26 @@ const popupData = [
 			"./images/TNMT_001.webp",
 			"./images/TNMT_002.webp",
 			"./images/TNMT_003.webp",
+			"./images/TNMT_004.webp",
+			"./images/TNMT_005.webp",
 			"./images/COLECTION_INTERIER.webp",
 			"./images/TNMT_INTERIER.webp",
 		],
 		oldPrice: "3500 руб.",
 		newPrice: "1650 руб.",
-		description: `Lorem ipsum dolor sit amet consectetur. Nam pellentesque amet
-					at&nbsp;quis bibendum. Duis feugiat et&nbsp;orci posuere velit
-					malesuada tempus. Ut&nbsp;vitae odio urna elementum urna enim.
-					Ut&nbsp;nisi diam purus augue id&nbsp;interdum malesuada sed massa.
-					Augue vitae volutpat nunc nisi fermentum in&nbsp;convallis orci.
-					Condimentum mattis eu&nbsp;pharetra sem quam facilisis&nbsp;id.
-					<br />
-					<br />
-					Ut&nbsp;nisi diam purus augue id&nbsp;interdum malesuada sed massa.
-					Augue vitae volutpat nunc nisi fermentum in&nbsp;convallis orci.
-					Condimentum mattis eu&nbsp;pharetra sem quam facilisis&nbsp;id.`,
+		description: `Присоединяйтесь к Черепашкам-ниндзя в их борьбе за справедливость! Этот набор позволит вам создать свою собственную арену для сражений, где, каждый из любимых персонажей обладает уникальными навыками, что делает битву непредсказуемой. Яркая графика перенесет вас в мир комиксов и мультфильмов. А кто ваш любимчик из легендарных черепашек?
+		<br />
+		<br />
+		Размер упаковки : 182х202х16 мм
+		<br />
+		Размер диорамы : 200х180х19мм
+		<br />
+		Количество деталей : 27 шт.
+		<br />
+		Время сборки : 30 мин.
+		<br />
+		Уровень сложности : средний
+`,
 
 		link: "https://www.ozon.ru",
 	},
@@ -139,25 +158,29 @@ const popupData = [
 		name: "Aladdin",
 		logo: "./images/marioLogo.png",
 		photos: [
-			"./images/M_001.webp",
-			"./images/M_002.webp",
-			"./images/M_003.webp",
-			"./images/COLECTION_INTERIER.webp",
-			"./images/M_INTERIER.webp",
+			"./images/A_001.webp",
+			"./images/A_002.webp",
+			"./images/A_003.webp",
+			"./images/A_002.webp",
+			"./images/A_003.webp",
+			"./images/A_BANG001.webp",
+			"./images/A_BANG002.webp",
 		],
 		oldPrice: "3500 руб.",
 		newPrice: "1650 руб.",
-		description: `Lorem ipsum dolor sit amet consectetur. Nam pellentesque amet
-					at&nbsp;quis bibendum. Duis feugiat et&nbsp;orci posuere velit
-					malesuada tempus. Ut&nbsp;vitae odio urna elementum urna enim.
-					Ut&nbsp;nisi diam purus augue id&nbsp;interdum malesuada sed massa.
-					Augue vitae volutpat nunc nisi fermentum in&nbsp;convallis orci.
-					Condimentum mattis eu&nbsp;pharetra sem quam facilisis&nbsp;id.
-					<br />
-					<br />
-					Ut&nbsp;nisi diam purus augue id&nbsp;interdum malesuada sed massa.
-					Augue vitae volutpat nunc nisi fermentum in&nbsp;convallis orci.
-					Condimentum mattis eu&nbsp;pharetra sem quam facilisis&nbsp;id.`,
+		description: `Сражения с врагами на загадочных улицах Аграбы, открывает двери в волшебный мир Аладдина! Сюжет игры разворачивается по событиям одноименного мультфильма! Собирая монеты и магические артефакты заставляют проявить ловкость и смекалку, чтобы преодолеть препятствия и спасти принцессу Жасмин. Соберите элементы, чтобы создать сцену с джином и магическим ковром.
+		<br />
+		<br />
+		Размер упаковки : 170х230х16 мм
+		<br />
+		Размер диорамы : 228х168х19мм
+		<br />
+		Количество деталей : 
+		<br />
+		Время сборки : 30 мин.
+		<br />
+		Уровень сложности : средний
+`,
 
 		link: "https://www.ozon.ru",
 	},
@@ -169,22 +192,26 @@ const popupData = [
 			"./images/M_001.webp",
 			"./images/M_002.webp",
 			"./images/M_003.webp",
+			"./images/CZ_004.webp",
+			"./images/CZ_005.webp",
 			"./images/COLECTION_INTERIER.webp",
 			"./images/M_INTERIER.webp",
 		],
 		oldPrice: "3500 руб.",
 		newPrice: "1650 руб.",
-		description: `Lorem ipsum dolor sit amet consectetur. Nam pellentesque amet
-					at&nbsp;quis bibendum. Duis feugiat et&nbsp;orci posuere velit
-					malesuada tempus. Ut&nbsp;vitae odio urna elementum urna enim.
-					Ut&nbsp;nisi diam purus augue id&nbsp;interdum malesuada sed massa.
-					Augue vitae volutpat nunc nisi fermentum in&nbsp;convallis orci.
-					Condimentum mattis eu&nbsp;pharetra sem quam facilisis&nbsp;id.
-					<br />
-					<br />
-					Ut&nbsp;nisi diam purus augue id&nbsp;interdum malesuada sed massa.
-					Augue vitae volutpat nunc nisi fermentum in&nbsp;convallis orci.
-					Condimentum mattis eu&nbsp;pharetra sem quam facilisis&nbsp;id.`,
+		description: `Видеоигра в виде аркадного экшена, погружает в красочный мир комиксов! Персонажи игры были перенесены с реальных людей методом оцифровки и технологией захвата движения motion capture, благодаря чему они двигались с великолепной для того времени реалистичностью. Воссоздайте стильный арт-объект, который вдохновит вас на новые креативные идеи и подчеркнет любовь к необычным играм.
+		<br />
+		<br />
+		Размер упаковки : 170х230х16 мм
+		<br />
+		Размер диорамы : 228х168х19мм
+		<br />
+		Количество деталей : 23 шт.
+		<br />
+		Время сборки : 30 мин.
+		<br />
+		Уровень сложности : начальный
+`,
 
 		link: "https://www.ozon.ru",
 	},
@@ -196,22 +223,26 @@ const popupData = [
 			"./images/M_001.webp",
 			"./images/M_002.webp",
 			"./images/M_003.webp",
+			"./images/EJ_004.webp",
+			"./images/EJ_005.webp",
 			"./images/COLECTION_INTERIER.webp",
 			"./images/M_INTERIER.webp",
 		],
 		oldPrice: "3500 руб.",
 		newPrice: "1650 руб.",
-		description: `Lorem ipsum dolor sit amet consectetur. Nam pellentesque amet
-					at&nbsp;quis bibendum. Duis feugiat et&nbsp;orci posuere velit
-					malesuada tempus. Ut&nbsp;vitae odio urna elementum urna enim.
-					Ut&nbsp;nisi diam purus augue id&nbsp;interdum malesuada sed massa.
-					Augue vitae volutpat nunc nisi fermentum in&nbsp;convallis orci.
-					Condimentum mattis eu&nbsp;pharetra sem quam facilisis&nbsp;id.
-					<br />
-					<br />
-					Ut&nbsp;nisi diam purus augue id&nbsp;interdum malesuada sed massa.
-					Augue vitae volutpat nunc nisi fermentum in&nbsp;convallis orci.
-					Condimentum mattis eu&nbsp;pharetra sem quam facilisis&nbsp;id.`,
+		description: `Вернуться в мир абсурдного юмора и приключений поможет набор Earthworm Jim II! Наверняка помните червяка, обладающего суперспособностями, который сражается с забавными врагами и решает головоломки!? Оригинальный стиль и неожиданные поворота сюжета, оставляют теплые воспоминания о путешествиях. Элементы набора напомнят о смешных моментах и добавят веселья в ваш интерьер.
+		<br />
+		<br />
+		Размер упаковки : 170х230х16 мм
+		<br />
+		Размер диорамы : 228х168х19мм
+		<br />
+		Количество деталей : 25 шт.
+		<br />
+		Время сборки : 30 мин.
+		<br />
+		Уровень сложности : начальный
+`,
 
 		link: "https://www.ozon.ru",
 	},
@@ -223,22 +254,26 @@ const popupData = [
 			"./images/M_001.webp",
 			"./images/M_002.webp",
 			"./images/M_003.webp",
+			"./images/S_004.webp",
+			"./images/S_005.webp",
 			"./images/COLECTION_INTERIER.webp",
 			"./images/M_INTERIER.webp",
 		],
 		oldPrice: "3500 руб.",
 		newPrice: "1650 руб.",
-		description: `Lorem ipsum dolor sit amet consectetur. Nam pellentesque amet
-					at&nbsp;quis bibendum. Duis feugiat et&nbsp;orci posuere velit
-					malesuada tempus. Ut&nbsp;vitae odio urna elementum urna enim.
-					Ut&nbsp;nisi diam purus augue id&nbsp;interdum malesuada sed massa.
-					Augue vitae volutpat nunc nisi fermentum in&nbsp;convallis orci.
-					Condimentum mattis eu&nbsp;pharetra sem quam facilisis&nbsp;id.
-					<br />
-					<br />
-					Ut&nbsp;nisi diam purus augue id&nbsp;interdum malesuada sed massa.
-					Augue vitae volutpat nunc nisi fermentum in&nbsp;convallis orci.
-					Condimentum mattis eu&nbsp;pharetra sem quam facilisis&nbsp;id.`,
+		description: `Скорость – это ваше второе имя! Встречайте антропоморфного ежа Соника! Самый быстрый борец за свободу, обладающий способностью бегать быстрее скорости звука. Соник пытается спасти животных, собирая кольца, избегая ловушек доктора Эггмана. Яркий акцент деталей, подчеркивает страсть к захватывающему сюжету и заставляет вспомнить тот самый саундтрек. Уже вспомнили?)
+		<br />
+		<br />
+		Размер упаковки : 170х230х16 мм
+		<br />
+		Размер диорамы : 228х168х19мм
+		<br />
+		Количество деталей : 27 шт.
+		<br />
+		Время сборки : 30 мин.
+		<br />
+		Уровень сложности : начальный
+`,
 
 		link: "https://www.ozon.ru",
 	},
@@ -250,22 +285,26 @@ const popupData = [
 			"./images/M_001.webp",
 			"./images/M_002.webp",
 			"./images/M_003.webp",
+			"./images/MK_004.webp",
+			"./images/MK_005.webp",
 			"./images/COLECTION_INTERIER.webp",
 			"./images/M_INTERIER.webp",
 		],
 		oldPrice: "3500 руб.",
 		newPrice: "1650 руб.",
-		description: `Lorem ipsum dolor sit amet consectetur. Nam pellentesque amet
-					at&nbsp;quis bibendum. Duis feugiat et&nbsp;orci posuere velit
-					malesuada tempus. Ut&nbsp;vitae odio urna elementum urna enim.
-					Ut&nbsp;nisi diam purus augue id&nbsp;interdum malesuada sed massa.
-					Augue vitae volutpat nunc nisi fermentum in&nbsp;convallis orci.
-					Condimentum mattis eu&nbsp;pharetra sem quam facilisis&nbsp;id.
-					<br />
-					<br />
-					Ut&nbsp;nisi diam purus augue id&nbsp;interdum malesuada sed massa.
-					Augue vitae volutpat nunc nisi fermentum in&nbsp;convallis orci.
-					Condimentum mattis eu&nbsp;pharetra sem quam facilisis&nbsp;id.`,
+		description: `Жесткие и динамичные бои 1 на 1, специальные приемы, комбо-комбинации. Конечно же это Мортал Комбат! Вспомните, когда пытались исполнить, смотря в книгу какой-то из приемов фаталити или бруталити, а может и знали пару приемов наизусть? Нервничали, когда проигрывали или наслаждались победой, когда жизнь персонажа оставалась на волоске. Открыть секретных персонажей и взойти на вершину башни. Это действительно тот самый ностальгический бит, который привлечет внимание.		
+		<br />
+		<br />
+		Размер упаковки : 170х230х16 мм
+		<br />
+		Размер диорамы : 228х168х19мм
+		<br />
+		Количество деталей : 24 шт.
+		<br />
+		Время сборки : 30 мин.
+		<br />
+		Уровень сложности : начальный
+`,
 
 		link: "https://www.ozon.ru",
 	},
